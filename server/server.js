326761151de8c52fe521948300fba2e5932d27d4
Cristian <coronel.cristian@yahoo.com.ar>
2019-10-25 00:47:47 -0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+require("./config/config");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get("/usuario", function(req, res) {
+  res.json("get usuario");
+});
+
+app.post("/usuario", function(req, res) {
+  let body = req.body;
+
+  if (body.nombre === undefined) {
+    res.status(400).json({
+      ok: false,
+      mensaje: "el nombre es necesario"
+    });
+  } else {
+    res.json({
+      body
+    });
+  }
+});
+
+app.delete("/usuario", function(req, res) {
+  res.json("delete usuario");
+});
+
+app.put("/usuario/:id", function(req, res) {
+  let id = req.params.id;
+  res.json({
+    id
+  });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`escuchando puerto ${process.env.PORT}`);
+});
