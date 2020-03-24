61b615a2d483e86f5a3cef74228fcee99e61b068
Cristian <coronel.cristian@yahoo.com.ar>
2020-03-24 11:02:01 -0300

    Variable de entorno Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -18,8 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
   urlDB = "mongodb://localhost:27017/cafe";
 } else {
-  urlDB =
-    "mongodb+srv://zumba:r1CV1DxRxKOfK4gk@cluster0-tgra1.mongodb.net/cafe";
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
